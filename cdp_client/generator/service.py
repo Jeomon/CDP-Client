e4e6a55c7d5df74ceca4ecc96932b1c9d6f0ea69
jeomon <jeogeoalukka@gmail.com>
2025-11-23 11:37:42 +0530

    feat: add CDPGenerator class for generating CDP client code.

--- cdp_client/generator/service.py
+++ cdp_client/generator/service.py
@@ -1,22 +1,23 @@
 from cdp_client.generator.constant import BROWSER_PROTOCOL_URL, JS_PROTOCOL_URL
 from cdp_client.generator.method_generator import MethodGenerator
-from cdp_client.generator.type_generator import TypeGenerator
 from cdp_client.generator.event_generator import EventGenerator
+from cdp_client.generator.type_generator import TypeGenerator
 from jinja2 import Template
 from textwrap import dedent
 from pathlib import Path
+from typing import Any
 import inflection
 import httpx
 
 class CDPGenerator:
     def __init__(self,path:Path=Path("./cdp_client/protocol")):
         self.path = path
-        self.method_generator = MethodGenerator(path=path)
         self.type_generator = TypeGenerator(path=path)
         self.event_generator = EventGenerator(path=path)
+        self.method_generator = MethodGenerator(path=path)
 
     @property
-    def domains(self)->list:
+    def domains(self)->list[dict[str,Any]]:
         domains=[]
         for url in [BROWSER_PROTOCOL_URL,JS_PROTOCOL_URL]:
             try:
@@ -34,6 +35,7 @@ class CDPGenerator:
 
     def generate_domain_service(self,domain:dict):
         domain_dir = self.path / inflection.underscore(domain['domain'])
+
         event_services_content=self.event_generator.generate_event_services(domain)
         method_services_content=self.method_generator.generate_method_services(domain)
 
@@ -42,6 +44,7 @@ class CDPGenerator:
 
     def generate_domain_types(self,domain:dict):
         domain_dir = self.path / inflection.underscore(domain['domain'])
+        
         inits_content=self.generate_inits(domain)
         types_content=self.type_generator.generate_types(domain)
         event_types_content=self.event_generator.generate_event_types(domain)
